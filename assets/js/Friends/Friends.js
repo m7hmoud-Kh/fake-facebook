cda5e7a65ceb1338854e50bb2cf85e5dfe026d66
Mohamed Mohei <mohamedmohei1920@gmail.com>
2022-11-05 03:08:08 +0200

    Finish Friends Page

---
+++ assets/js/Friends/Friends.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween:0,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
