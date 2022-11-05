7efb20aec9b73bf41975e1670aa211b129ac2d0b
Mohamed Mohei <mohamedmohei1920@gmail.com>
2022-11-05 15:04:20 +0200

    Fix Slider and Card Width

--- assets/js/Friends/Friends.js
+++ assets/js/Friends/Friends.js
@@ -1,8 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween:0,
-  loop: true,
-  loopFillGroupWithBlank: true,
+  slidesPerView: 4,
+  spaceBetween:10,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
